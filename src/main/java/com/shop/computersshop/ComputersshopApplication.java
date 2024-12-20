408cb201fa89f29e0cb1616e6f1e432be3518955
OlenaMyroshnykova <elenamirespania@gmail.com>
2024-12-20 12:09:41 +0100

    create java project

---
+++ src/main/java/com/shop/computersshop/ComputersshopApplication.java
@@ -0,0 +1,13 @@
+package com.shop.computersshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComputersshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComputersshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shop/computersshop/ComputersshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shop.computersshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComputersshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
